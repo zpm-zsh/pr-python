34a84cdcca689915e77d86e5324bd5f6a84e8e50
Grigorii Horos <horosgrisa@gmail.com>
2023-08-18 00:10:57 +0300

    Change ui

---
+++ pr-python.plugin.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+: ${PR_PYTHON_SYMBOL:=''}
+
+typeset -g pr_python
+
+VIRTUAL_ENV_DISABLE_PROMPT=1
+
+if [[ -n "$VIRTUAL_ENV" ]]; then
+  source "$VIRTUAL_ENV/bin/activate"
+fi
+
+if (( $+commands[python] )); then
+  function _pr_python() {
+    pr_python=''
+
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+      pr_python=" %{${c[base3]}${c[bold]}%}(%{${c[reset]}%}"
+
+      pyver=$(python --version)
+      pyver=${pyver#'Python '}
+
+      pr_python+="%{${c[blue]}${c[bold]}%}${PR_PYTHON_SYMBOL}%{${c[reset]}%} %{${c[blue]}${c[bold]}%}$pyver%{${c[reset]}%}"
+
+      pr_python+="%{${c[base3]}${c[bold]}%})%{${c[reset]}%}"
+    fi
+  }
+
+  autoload -Uz add-zsh-hook
+  add-zsh-hook chpwd _pr_python
+  _pr_python
+fi
